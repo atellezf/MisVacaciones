4eb57b4b12de79a6c8cf2d3afc0d4fcb05fea191
atellezfx <atellezfx@correo.com>
2021-09-29 21:18:32 -0500

    Se integra código de prueba de carga de interfaz gráfica

--- src/mx/utel/poo/misvac/ProgramaGUI.java
+++ src/mx/utel/poo/misvac/ProgramaGUI.java
@@ -4,4 +4,12 @@ import javax.swing.*;
 
 public class ProgramaGUI {
     private JPanel rootPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ProgramaGUI");
+        frame.setContentPane(new ProgramaGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
