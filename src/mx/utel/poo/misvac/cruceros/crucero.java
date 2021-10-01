2d9f0d777475a4a4c76ca927bbe7e8796fd2bf82
ponandi <ponandi@hotmail.com>
2021-09-30 21:27:51 -0500

    PRIMER PROGRAMA OPEN CLASS VACACIONES PONANDI

---
+++ src/mx/utel/poo/misvac/cruceros/crucero.java
@@ -0,0 +1,61 @@
+package mx.utel.poo.misvac.cruceros;
+
+public class crucero {
+
+        private String naviera;
+        private String destino;
+        private int noches;
+        private String categoria;
+
+    public String getNaviera() {
+        return naviera;
+    }
+
+    public void setNaviera(String naviera) {
+        this.naviera = naviera;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getNoches() {
+        return noches;
+    }
+
+    public void setNoches(int noches) {
+        this.noches = noches;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+
+
+    public crucero(String naviera, String destino, int noches, String categoria) {
+        this.naviera = naviera;
+        this.destino = destino;
+        this.noches = noches;
+        this.categoria = categoria;
+    }
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "\tcrucero: %s/%s %d año(s)\n".formatted(naviera, destino, noches,categoria);
+    }
+}
