272a329b9d0c98f34aebde306fa23058b579b3b3
ponandi <ponandi@hotmail.com>
2021-09-30 23:46:02 -0500

    se crea codigo Aereo by Pablo Onandi

---
+++ src/mx/utel/poo/misvac/aereos/Aereo.java
@@ -0,0 +1,45 @@
+package mx.utel.poo.misvac.aereos;
+
+public class Aereo
+{
+
+    private String origen;
+    private String destino;
+    private String horaSalida;
+    private String horaLlegada;
+
+    public Aereo(String origen, String destino, String horaSalida, String horaLlegada)
+    {
+        this.origen = origen;
+        this.destino = destino;
+        this.horaSalida = horaSalida;
+        this.horaLlegada = horaLlegada;
+    }
+
+    public String getOrigen() {return origen;}
+
+    public void setOrigen(String origen) {this.origen = origen;}
+
+    public String getDestino() {return destino;}
+
+    public void setDestino(String destino) {this.destino = destino;}
+
+    public String getHoraSalida() {return horaSalida;}
+
+    public void setHoraSalida(String horaSalida) {this.horaSalida = horaSalida;}
+
+    public String getHoraLlegada() {return horaLlegada;}
+
+    public void setHoraLlegada(String horaLlegada) {this.horaLlegada = horaLlegada;}
+
+    @Override
+    public String toString() {
+        return "Aereo{" +
+                "origen='" + origen + '\'' +
+                ", destino='" + destino + '\'' +
+                ", horaSalida='" + horaSalida + '\'' +
+                ", horaLlegada='" + horaLlegada + '\'' +
+                '}';
+    }
+}
+
